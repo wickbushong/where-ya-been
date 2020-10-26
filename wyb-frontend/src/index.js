f89d563130215baadf3484ea59221ac22679c688
wickbushong <wickbushong@gmail.com>
2020-10-26 17:30:30 -0500

    Add current list to main doc. Start function to fetch current visitors.

--- wyb-frontend/src/index.js
+++ wyb-frontend/src/index.js
@@ -1,1 +1,8 @@
 const BACKEND_URL = 'localhost:3000';
+
+function getCurrentList() {
+    // NEEDS TO BE DYNAMIC URL
+    fetch(BACKEND_URL+`businesses/1`)
+        .then(response => response.json())
+            .then(data => console.log(data));
+}
