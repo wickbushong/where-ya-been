fc962aeeac7fc7d06c8fc306cb17e2d6f6e8478a
wickbushong <wickbushong@gmail.com>
2020-10-27 15:20:35 -0500

    Add active visits to current list and add check out button to list items.

--- wyb-frontend/src/index.js
+++ wyb-frontend/src/index.js
@@ -1,8 +1,37 @@
-const BACKEND_URL = 'localhost:3000';
+const BACKEND_URL = 'http://localhost:3000';
 
-function getCurrentList() {
+document.addEventListener("DOMContentLoaded", fetchActiveVisits())
+
+function fetchActiveVisits() {
     // NEEDS TO BE DYNAMIC URL
-    fetch(BACKEND_URL+`businesses/1`)
+    fetch(BACKEND_URL+`/businesses/1`)
         .then(response => response.json())
-            .then(data => console.log(data));
+            .then(data => createCurrentList(data["active_visits"]))
+                .catch(err => console.log(err));
 }
+
+function appendVisitToCurrentList(visit) {
+    let ul = document.querySelector("#current-list")
+    let li = document.createElement("li")
+    li.className = "list-group-item"
+    li.setAttribute("data-visit-id", `${visit.id}`)
+    li.setAttribute("data-user-id", `${visit.user.id}`)
+    li.innerHTML = `${visit.user.first_name} ${visit.user.last_name}`
+    let btn = document.createElement("button")
+    btn.className = "btn btn-outline-danger btn-sm float-right"
+    btn.innerHTML = "CHECK OUT"
+    btn.addEventListener("click", e => checkOutVisit(e))
+    li.appendChild(btn)
+    ul.appendChild(li)
+}
+
+function createCurrentList(visits) {
+    for (const visit of visits) {
+        appendVisitToCurrentList(visit)
+    }
+}
+
+function checkOutVisit(e) {
+    debugger
+}
+
