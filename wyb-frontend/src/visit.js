6eff435a3c755298fd834dec030e0c22a5d785a6
wickbushong <wickbushong@gmail.com>
2020-10-28 14:15:28 -0500

    Add visit class and create individual javascript files for classes.

---
+++ wyb-frontend/src/visit.js
@@ -0,0 +1,11 @@
+class Visit {
+    constructor(visit_obj) {
+        this.id = visit_obj["id"]
+        this.user_id = visit_obj["user_id"]
+        this.business_id = visit_obj["business_id"]
+        this.time_in = visit_obj["time_in"]
+        this.time_out = visit_obj["time_out"]
+    }
+
+    
+}
