3d1e48bb5963cc44f61b939c186182867c52b761
wickbushong <wickbushong@gmail.com>
2020-10-30 16:12:31 -0500

    Build Report post instance method.

--- wyb-frontend/src/report.js
+++ wyb-frontend/src/report.js
@@ -1,4 +1,9 @@
 class Report {
+    constructor(report_obj) {
+        this.id = report_obj["id"]
+        this.user_id = report_obj["user_id"]
+        this.test_date = report_obj["test_date"]
+    }
 
     static activateTab() {
         document.querySelector("#report-nav").className = "nav-link nav-tab"
@@ -10,13 +15,29 @@ class Report {
         let form = document.querySelector("#report-form form")
         form.addEventListener("submit", e => {
             e.preventDefault()
-            debugger
+            let select = e.target.querySelector("select")
+            let selectedUser = select.item(select.selectedIndex)
+            let userId = selectedUser.dataset.userId
+            let date = e.target.querySelector("input").value
+            new this({test_date: date, user_id: userId}).post()
         })
         document.querySelector("#report-activator").style = "display: none;"
         document.querySelector("#report-body").style = "display: block;"
     }
 
     post() {
-
+        fetch(`http://localhost:3000/users/${this.user_id}/reports`, {
+                method: 'POST',
+                headers:  {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
+                },
+                body: JSON.stringify(this)
+            })
+            .then(response => response.json())
+                .then(result => {
+                    console.log(result)
+                })
+                    .catch(err => console.log(err))
     }
 }
